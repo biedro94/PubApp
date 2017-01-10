a3a6e55cfb9b554e4c992d8aa0bf4e43b6ed5501
Brzoizkowy <sin0xxd@gmail.com>
2017-01-10 17:06:34 +0100

    Create poject app
    project

---
+++ MobileApp/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { StatusBar } from 'ionic-native';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ListPage } from '../pages/list/list';
+import { RegistrationPage } from '../pages/registration/registration';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = RegistrationPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    public platform: Platform,
+    public menu: MenuController
+  ) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Menu', component: HelloIonicPage },
+      { title: 'Zamówienia', component: ListPage },
+      { title: 'Rejestracja', component: RegistrationPage}
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
+1111111111111111
